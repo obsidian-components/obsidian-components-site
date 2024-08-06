6d54f047177f6ead47b84c189b68972758f23ba1
vran <vran_dev@foxmail.com>
2024-08-06 17:21:20 +0800

    fix: product url incorrect

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   // favicon: '',
 
   // Set the production url of your site here
-  url: 'https://vran.github.io',
+  url: 'https://obsidian-components.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/obsidian-components-site',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
